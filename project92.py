acfa76b87100fbe1ad192abe023c134ddc58f9f1
Akshay Saini <72139061+aakshay001@users.noreply.github.com>
2021-08-05 18:27:36 +0530

    Update project92.py

--- project92.py
+++ project92.py
@@ -30,11 +30,11 @@ option = st.selectbox("Enter Your Choice",("Calculate EMI","Outstanding Balance"
 if st.button("Calculate"):
 	if option == "Calculate EMI":
 		emi = calculate_emi(p,tenure/12,roi)
-	        st.write("Calculated EMI is: ",emi)
+	st.write("Calculated EMI is: ",emi)
 		
 	elif option ==  "Calculate Outstanding Balance":
 		balance = calculate_outstanding_balance(p,tenure/12,roi,m)
-	        st.write("Outstanding Balance: ",balance)
+	st.write("Outstanding Balance: ",balance)
 		
 	
 
